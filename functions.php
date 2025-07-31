1c979acb9a7341cf76237f0590eb5e516197df90
Chris Kinsman <chrisknsmn@gmail.com>
2025-07-31 11:28:24 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,44 +5,62 @@ function enqueue_child_theme_styles() {
 add_action('wp_enqueue_scripts', 'enqueue_child_theme_styles');
 
 function register_custom_blocks() {
-    // My Simple Block
-    wp_register_script(
-        'my-block',
-        get_stylesheet_directory_uri() . '/assets/my-block.js',
-        array('wp-blocks', 'wp-element', 'wp-block-editor'),
-        filemtime(get_stylesheet_directory() . '/assets/my-block.js')
-    );
+    // Helper function to get file version safely
+    function get_file_version($file_path) {
+        return file_exists($file_path) ? filemtime($file_path) : '1.0.0';
+    }
 
-    wp_register_style(
-        'my-block-style',
-        get_stylesheet_directory_uri() . '/assets/my-block.css',
-        array(),
-        filemtime(get_stylesheet_directory() . '/assets/my-block.css')
-    );
+    $stylesheet_dir = get_stylesheet_directory();
+    $stylesheet_uri = get_stylesheet_directory_uri();
 
-    register_block_type('my-theme/simple-block', array(
-        'editor_script' => 'my-block',
-        'style' => 'my-block-style',
-    ));
+    // My Simple Block (only register if files exist)
+    $my_block_js = $stylesheet_dir . '/assets/my-block.js';
+    $my_block_css = $stylesheet_dir . '/assets/my-block.css';
+    
+    if (file_exists($my_block_js) && file_exists($my_block_css)) {
+        wp_register_script(
+            'my-block',
+            $stylesheet_uri . '/assets/my-block.js',
+            array('wp-blocks', 'wp-element', 'wp-block-editor'),
+            get_file_version($my_block_js)
+        );
+
+        wp_register_style(
+            'my-block-style',
+            $stylesheet_uri . '/assets/my-block.css',
+            array(),
+            get_file_version($my_block_css)
+        );
+
+        register_block_type('my-theme/simple-block', array(
+            'editor_script' => 'my-block',
+            'style' => 'my-block-style',
+        ));
+    }
 
     // Mac Template Block
-    wp_register_script(
-        'mac-template',
-        get_stylesheet_directory_uri() . '/assets/mac-template.js',
-        array('wp-blocks', 'wp-element', 'wp-block-editor'),
-        filemtime(get_stylesheet_directory() . '/assets/mac-template.js')
-    );
+    $mac_template_js = $stylesheet_dir . '/assets/mac-template.js';
+    $mac_template_css = $stylesheet_dir . '/assets/mac-template.css';
+    
+    if (file_exists($mac_template_js) && file_exists($mac_template_css)) {
+        wp_register_script(
+            'mac-template',
+            $stylesheet_uri . '/assets/mac-template.js',
+            array('wp-blocks', 'wp-element', 'wp-block-editor'),
+            get_file_version($mac_template_js)
+        );
 
-    wp_register_style(
-        'mac-template-style',
-        get_stylesheet_directory_uri() . '/assets/mac-template.css',
-        array(),
-        filemtime(get_stylesheet_directory() . '/assets/mac-template.css')
-    );
+        wp_register_style(
+            'mac-template-style',
+            $stylesheet_uri . '/assets/mac-template.css',
+            array(),
+            get_file_version($mac_template_css)
+        );
 
-    register_block_type('my-theme/mac-template', array(
-        'editor_script' => 'mac-template',
-        'style' => 'mac-template-style',
-    ));
+        register_block_type('my-theme/mac-template', array(
+            'editor_script' => 'mac-template',
+            'style' => 'mac-template-style',
+        ));
+    }
 }
 add_action('init', 'register_custom_blocks');
